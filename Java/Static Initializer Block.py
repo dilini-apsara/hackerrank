1fe2f2abcb8ca2e5bd94e2db2fadfc1e4a6185dc
Dilini Apsara <122234046+dilini-apsara@users.noreply.github.com>
2023-02-25 00:21:22 +0530

    Create Static Initializer Block.py

---
+++ Java/Static Initializer Block.py
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+public static Scanner scan =new Scanner(System.in); 
+  
+//static initializer method executed before the main() method
+  
+static int B=scan.nextInt();
+static int H=scan.nextInt();
+static boolean flag;
+ static {
+ 
+if (B>0 && H>=0){
+    flag=true;
+}else{
+    System.out.print("java.lang.Exception: Breadth and height must be positive");
+}
+
+}
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
+
